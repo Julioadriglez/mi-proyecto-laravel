e17e2c029d5c3bcbe60cf533d3c0cdf62409df83
Julio Adrian Gonzalez <julio.adri.glez.u@outlook.com>
2025-11-26 22:22:55 -0600

    Se cabio nombre de h1 y se agrego boton php

--- resources/views/imagen.blade.php
+++ resources/views/imagen.blade.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Imagen</title>
 </head>
 <body>
     <h1>Laravel</h1>
     <img   width="400" height="341" src="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/ibm.svg" alt="">
     <br>
-    <button target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/">Home</a></button>        
-    <button target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/inicial">Inicial</a></button>
+    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/">Home</a></button>        
+    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/inicial">Inicial</a></button>
+    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/php">PHP</a></button>
 </body>
 </html>
