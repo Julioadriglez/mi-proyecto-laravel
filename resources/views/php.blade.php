22a3c8a3c0140cc71d10feede80070f13698eccc
Julio Adrian Gonzalez <julio.adri.glez.u@outlook.com>
2025-11-26 22:26:33 -0600

    se agregaron los botones home, php e inicial en php

--- resources/views/php.blade.php
+++ resources/views/php.blade.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP</title>
 </head>
 <body>
     <h1>PHP</h1>
     <p>PHP (oficialmente, este sigle es un acrónimo recursivo para PHP Hypertext Preprocessor) es un lenguaje de scripts generalista y Open Source, especialmente concebido para el desarrollo de aplicaciones web. Puede ser integrado fácilmente al HTML.</p>
-    
+    <br>
+    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/">Home</a></button>        
+    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/imagen">Imagen</a></button>
+    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/inicial">Inicial</a></button>
 </body>
 </html>
