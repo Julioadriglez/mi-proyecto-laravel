a9432a229911391144c5dd59853f6195b8716863
Julio Adrian Gonzalez <julio.adri.glez.u@outlook.com>
2025-11-26 22:23:27 -0600

    Se cambio el title

--- resources/views/imagen.blade.php
+++ resources/views/imagen.blade.php
@@ -6,7 +6,7 @@
     <title>Imagen</title>
 </head>
 <body>
-    <h1>Laravel</h1>
+    <h1>Imagen</h1>
     <img   width="400" height="341" src="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/ibm.svg" alt="">
     <br>
     <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/">Home</a></button>        
