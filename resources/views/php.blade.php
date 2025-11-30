c3581324928f0fac1f4e99fb65f593e32c21c233
Julio Adrian Gonzalez <julio.adri.glez.u@outlook.com>
2025-11-30 17:31:01 -0600

    se creo la vista php,se  elimino codigo y  agrego lo necesario para su funcionamiento

--- resources/views/php.blade.php
+++ resources/views/php.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
-</head>
-<body>
-    <h1>PHP</h1>
-    <p>PHP (oficialmente, este sigle es un acrónimo recursivo para PHP Hypertext Preprocessor) es un lenguaje de scripts generalista y Open Source, especialmente concebido para el desarrollo de aplicaciones web. Puede ser integrado fácilmente al HTML.</p>
-    <br>
-    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/">Home</a></button>        
-    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/imagen">Imagen</a></button>
-    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/inicial">Inicial</a></button>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1 style="color: #16C809">PHP</h1>
+    <p style="color: #16C809">PHP (oficialmente, este sigle es un acrónimo recursivo para PHP Hypertext Preprocessor) es un lenguaje de scripts generalista y Open Source, especialmente concebido para el desarrollo de aplicaciones web. Puede ser integrado fácilmente al HTML.</p>
+
+@endsection
