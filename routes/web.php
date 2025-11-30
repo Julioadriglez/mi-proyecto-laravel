45c69bec8845fedf1c93ee408e415b942ce85a26
Julio Adrian Gonzalez <julio.adri.glez.u@outlook.com>
2025-11-30 17:26:14 -0600

    se agregaron las rutas de las vitas a web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SumaController;
+use App\Http\Controllers\SaludoController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,18 +13,20 @@ Route::get('/inicial', function () {
     return view('inicial');
 });
 
-Route::get('/imagen', function () {
-    return view('imagen');
-});
+// Route::get('/imagen', function () {
+//     return view('imagen');
+// });
 
-Route::get('/php', function () {
+/* Route::get('/php', function () {
     return view('php');
-});
+}); */
 
 Route::get('/suma', [sumaController::class,'index']);
 
 Route::post('/suma', [sumaController::class,'suma']);
 
-Route::get('/saludos', function () {
-    return view('saludos');
-});
+Route::get('/saludo', [SaludoController::class, 'index'])->name('index');
+
+Route::get('/imagen', [SaludoController::class, 'imagen'])->name('imagen');
+
+Route::get('/php', [SaludoController::class, 'php'])->name('php');
