203f9aa4d8a054de5be7e24903bc38756bca0389
Julio Adrian Gonzalez <julio.adri.glez.u@outlook.com>
2025-11-30 17:29:56 -0600

    se creo la vista imagen y elimino codigo y se agrego lo necesario para su funcionamiento

--- resources/views/imagen.blade.php
+++ resources/views/imagen.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Imagen</title>
-</head>
-<body>
-    <h1>Imagen</h1>
+@extends('layouts.app')
+
+@section('content')
+    <h1 style="color: #16C809">Imagen</h1>
     <img   width="400" height="341" src="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/ibm.svg" alt="">
-    <br>
-    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/">Home</a></button>        
-    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/inicial">Inicial</a></button>
-    <button  class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"> <a href="/php">PHP</a></button>
-</body>
-</html>
+@endsection
