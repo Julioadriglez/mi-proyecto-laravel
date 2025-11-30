e32ebef6fad0945cb4b637d22e6bd98843935d67
Julio Adrian Gonzalez <julio.adri.glez.u@outlook.com>
2025-11-30 17:27:53 -0600

    se agregaron las tres funciones para conectar con las tres vistas salidos, imagen y php

--- app/Http/Controllers/SaludoController.php
+++ app/Http/Controllers/SaludoController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class SaludoController extends Controller
 {
-    //
+     public function index()
+    {
+        // coneccion del controlador con la vista 'saludo'
+        return view('saludos');
+    }
+    public function imagen()
+    {
+        // coneccion del controlador con la vista 'saludo'
+        return view('imagen');
+    }
+    public function php()
+    {
+        // coneccion del controlador con la vista 'saludo'
+        return view('php');
+    }
 }
