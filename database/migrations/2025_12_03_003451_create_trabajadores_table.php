e545dd0ff0fddcf8ec3f4fec3820ed9acd2d5652
Julio Adrian Gonzalez <julio.adri.glez.u@outlook.com>
2025-12-02 18:40:28 -0600

    definicion de las columnas de la DB

--- database/migrations/2025_12_03_003451_create_trabajadores_table.php
+++ database/migrations/2025_12_03_003451_create_trabajadores_table.php
@@ -14,6 +14,15 @@ return new class extends Migration
         Schema::create('trabajadores', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('curp')->unique(); 
+            $table->string('telefono')->nullable();
+            $table->string('email')->unique();
+            $table->date('fecha_nacimiento');
+            $table->string('direccion')->nullable();
+            $table->string('puesto');
+            $table->decimal('salario', 10, 2);
         });
     }
 
