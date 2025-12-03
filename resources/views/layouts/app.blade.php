253edf22bc466483588f01f4aaea375b6761187d
Julio Adrian Gonzalez <julio.adri.glez.u@outlook.com>
2025-12-03 13:53:32 -0600

    Se agregaron los la estiquetas de bootstrap, la barra nav con css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,41 @@
     <style>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content_Type" content="text/html; charset=utf-8">
     <title>Proyecto</title>
 </head>
 <body style="background-color: rgb(0, 0, 0); font-family: monospace;">
-    <a href="{{ route('index') }}">saludo</a>
-    <a href="{{ route('imagen') }}">imagen</a> 
-    <a href="{{ route('php') }}">php</a> 
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="{{ route('index') }}">Saludos</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="{{ route('imagen') }}">Imagen</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('php') }}">PHP</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="{{ route('trabajadores') }}"  aria-disabled="true">Trabajadores</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+        
     @yield('content')
     <p style="color: #16C809">Hola, soy una plantilla Base</p>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </body>
 </html>
